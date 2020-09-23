648a480d7c5bd5579437377dc35d97210903b651
Danyelle Angelo <danyangelofsa@gmail.com>
2020-09-23 22:18:21 +0000

    Jogo

--- script.js
+++ script.js
@@ -1,42 +1,91 @@
 function preload() {
-    this.load.image('backgroundmap','assets/MapaGrama.png');
+    //fundos
+    this.load.image('backgroundmap','assets/Mapas/MapaGrama.png');
+    
+    //elementos
     this.load.image('tree','assets/tree01.png');
 
-  //=========================
-    //pré carregamento
-    this.load.image('player', 'assets/frente.png');
+    //personagem
+    //TODO arrumar tamanho da imagem
+    this.load.spritesheet('player','assets/personagem/personagem.png', { frameWidth:46, frameHeight:48 })
     
 }
 
 function create() {
      /*fundo*/
     this.add.image(250,200,'backgroundmap').setScale(2.0,2.0);
-
-  //=============================================
-    /*controles*/
-    this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
-    this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
-    this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
-    this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
    
     /*personagem*/
-    const player = this.player = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(1.0, 1.0);
-    this.player.setCollideWorldBounds(true);
-    /*objetos*/
+    var player = this.physics.add.sprite(config.width / 2, config.height / 2, 'player');
+    /*colisões*/
+    player.setCollideWorldBounds(true);
+    
+    /*Movimentos do personagem*/
+    this.anims.create({
+      key: 'stopped',
+      frames: [{key:"player",frame:5}],
+      framerate:20,
+    });
+    /*go*/
+    this.anims.create({
+      key:'goLeft',
+      frames: this.anims.generateFrameNumbers('player',{start:3,end:4}),
+      framerate:10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key:'goRight',
+      frames: this.anims.generateFrameNumbers('player',{start:1,end:2}),
+      framerate:10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key:'goUp',
+      frames: this.anims.generateFrameNumbers('player',{start:0,end:0}),
+      framerate:10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key:'goDown',
+      frames: this.anims.generateFrameNumbers('player',{start:5,end:5}),
+      framerate:10,
+      repeat: 1,
+    });
+      
+    /*others objetos*/
     var tree = this.physics.add.staticGroup();
     tree.create(100,100,'tree');
-    /*colisao*/
+    /*colisões*/
     this.physics.add.collider(tree, player)
-
+    this.player = player;
     
 }
 
 function update() {
     let cursors = this.input.keyboard.createCursorKeys();
-    if ((cursors.left.isDown || this.a.isDown) || (cursors.right.isDown || this.d.isDown)) this.player.setVelocityX(cursors.left.isDown || this.a.isDown ? -160 : 160);
-    else this.player.setVelocityX(0);
-    if ((cursors.up.isDown || this.w.isDown) || (cursors.down.isDown || this.s.isDown)) this.player.setVelocityY(cursors.up.isDown || this.w.isDown ? -160 : 160);
-    else this.player.setVelocityY(0);
+    var player = this.player;
+
+    /*go*/
+    if(cursors.left.isDown){
+      player.setVelocityX(-100);
+      player.anims.play('goLeft', true);
+    }else if(cursors.right.isDown){
+      player.setVelocityX(100);
+      player.anims.play('goRight', true);
+    }else if(cursors.up.isDown){
+      player.setVelocityY(-100);
+      player.anims.play('goUp', true);
+    }else if(cursors.down.isDown){
+      player.setVelocityY(100);
+      player.anims.play('goDown');
+    }else{
+      player.setVelocityX(0);
+      player.setVelocityY(0);
+      player.anims.play('stopped');
+    }
 }
 
 const config = {
@@ -51,7 +100,7 @@ const config = {
             gravity: {
                 y: 0
             },
-            debug: false
+            debug: true
         }
     },
     scene: {
