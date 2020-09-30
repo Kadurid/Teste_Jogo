065a39d42a3dbb34152951e194f5963ea20bdee5
Danyelle Angelo <danyangelofsa@gmail.com>
2020-09-30 22:47:54 +0000

    versão estável

--- fireMission.js
+++ fireMission.js
@@ -1,39 +1,14 @@
-//TODO problema na execução:funções não esperam o término das outras. ver async e wait 
-
-function FireMission(game){
-    this.game = game;
-    this.tree = game.treeFireMission; 
-    this.campfire = game.physics.add.sprite(config.width / 2, config.height / 2, 'elements').setImmovable().setInteractive();
-    this.goal = 100;
-    this.incrementProgress = 5;
-    this.progress = 0;
-    this.steps = {
-        "collectingMaterial":{"goal":this.goal/2,"msg":"UMMMMMMMMMM....essa árvore parece ótima, vamos começar por aqui, clique sobre ela para coletar os galhos e as folhas mais secas","obj":this.tree},
-        "buildCampfire":{"goal":this.goal,"msg":"clique nos galhos. colocar explicacao",
-        "obj":this.campfire},
-    };
-    
-    this.campfire.setActive(false).setVisible(false);
-
-    collectingMaterial(this);
-    buildCampfire(this);
-    
-
+function fireMission(player,tree){
+    //TODO exibir mensagem na tela falando que o user achou uma árvore perfeita e instruir o usuário a clicar na tela até que seja coletado lenha
+    var progressFireMission = 0;
+    alert("Clique a árvore repetidas vezes para obter galhos e folhas secas para fazer fogo");
+    tree.on('pointerdown', function() {
+        if(progressFireMission<100){
+             progressFireMission +=10;
+            //TODO exibir barra de progress noo lugar de console.log
+            console.log("Progresso em obter lenha:"+progressFireMission);
+        }
+    });
 }
 
-function collectingMaterial(mission){
-    clickObject(mission.game,mission.progress,mission.incrementProgress,mission.steps["collectingMaterial"]);
-}
-
-function buildCampfire(mission){
-        //TODO mudar para tercera galho,colcar vários
-        //TDO fazer galhoo aparecer suavemete 
-
-        this.campfire.setActive(true).setVisible(true);
-        mission.game.physics.add.collider(mission.campfire, mission.game.player);
-
-        //TODO corrigir Movimentos
-        mission.game.player.body.velocity.x += 3000;
-        clickObject(mission.game,mission.progress,mission.incrementProgress,mission.steps["buildCampfire"]);
-        if(mission.progress == 100)mission.campfire.setTexture('elements',15);
-}
+module.exports = {fireMission};
