ebb5c36ef8222981737611572240371a0604cc91
Carlos Eduardo Pereira Santana <kaduridu@gmail.com>
2020-11-04 16:31:45 +0000

    debug para camada chão

--- scenes/MainScene.js
+++ scenes/MainScene.js
@@ -25,7 +25,12 @@ class MainScene extends Phaser.Scene {
             collidingTileColor: new Phaser.Display.Color(243,234,48),
             faceColor: new Phaser.Display.Color(40,39,37,255)
         });
-
+        groundLayer.renderDebug(debugGraphics,{
+          tileColor: null,
+          collidingTileColor: new Phaser.Display.Color(205,65,58),
+          faceColor: new Phaser.Display.Color(40,39,37,255)
+        });
+      
     }
     
     update(){
